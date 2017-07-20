fee5ded83e0277991669866668fc82779e188a54
jiali.liu <jiali.liu@ck-telecom.com>
2017-07-20 13:14:21 +0800

    Review_screen_onoff:+6case

--- src/one/hardware/Testcase/Review_Screen_onoff.java
+++ src/one/hardware/Testcase/Review_Screen_onoff.java
@@ -7,17 +7,16 @@ package one.hardware.Testcase;
 import com.ckt.demo.UiAutomatorHelper;
 import android.view.KeyEvent;
 import com.android.uiautomator.core.UiDevice;
+
+import one.hardware.Action.CameraAction;
 import one.hardware.Util.Base;
 
 
 public class Review_Screen_onoff extends Base{
 
 
-	public void testReview_Screen_onoff() throws Exception  {
-		try{
-			initUIAutomator(this.getName());
-			common.startLog("*****Start to run " + runcase + " *****");    			
-			common.startCamera();		  
+	private void Review_Screen_onoff() throws Exception  {
+		try{		  
 
 			UiDevice.getInstance().pressKeyCode(KeyEvent.KEYCODE_POWER);         //按电源键6次
 			sleep(1000);
@@ -59,6 +58,48 @@ public class Review_Screen_onoff extends Base{
 		}				
 	}	
 
+	public void testLiveMode_Screen_onoff() throws Exception{
+		initUIAutomator(this.getName());
+		common.startLog("*****Start to run " + runcase + " *****");    			
+		common.startCamera();
+		CameraAction.navconfig(one.hardware.Page.Camera.nav_menu[0]);
+		Review_Screen_onoff();
+	}
+	public void testVideoMode_Screen_onoff() throws Exception{
+		initUIAutomator(this.getName());
+		common.startLog("*****Start to run " + runcase + " *****");    			
+		common.startCamera();
+		CameraAction.navconfig(one.hardware.Page.Camera.nav_menu[1]);
+		Review_Screen_onoff();
+	}
+	public void testCaptureMode_Screen_onoff() throws Exception{
+		initUIAutomator(this.getName());
+		common.startLog("*****Start to run " + runcase + " *****");    			
+		common.startCamera();
+		CameraAction.navconfig(one.hardware.Page.Camera.nav_menu[2]);
+		Review_Screen_onoff();
+	}
+	public void testBurstMode_Screen_onoff() throws Exception{
+		initUIAutomator(this.getName());
+		common.startLog("*****Start to run " + runcase + " *****");    			
+		common.startCamera();
+		CameraAction.navconfig(one.hardware.Page.Camera.nav_menu[3]);
+		Review_Screen_onoff();
+	}
+	public void testSlo_MOMode_Screen_onoff() throws Exception{
+		initUIAutomator(this.getName());
+		common.startLog("*****Start to run " + runcase + " *****");    			
+		common.startCamera();
+		CameraAction.navconfig(one.hardware.Page.Camera.nav_menu[4]);
+		Review_Screen_onoff();
+	}
+	public void testLapseMode_Screen_onoff() throws Exception{
+		initUIAutomator(this.getName());
+		common.startLog("*****Start to run " + runcase + " *****");    			
+		common.startCamera();
+		CameraAction.navconfig(one.hardware.Page.Camera.nav_menu[5]);
+		Review_Screen_onoff();
+	}
 	public static void main(String[] args) {
 		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.Review_Screen_onoff ", "testReview_Screen_onoff()", "7");		
 	}
