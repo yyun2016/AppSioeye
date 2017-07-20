d0edc0389a60eb206bdea99ec6fc67ec941a7da3
yyun2016 <yun.yang@outlook.com>
2017-07-20 21:33:28 +0800

    +BurstCase

--- src/one/hardware/Testcase/BurstCase.java
+++ src/one/hardware/Testcase/BurstCase.java
@@ -10,7 +10,13 @@ import one.hardware.Page.Camera;
 import one.hardware.Util.Base;
 
 public class BurstCase extends Base{
-	
+	/**
+	 * 测试照片质量18M 10张连拍
+	 * 1.检查是否有增加10张照片
+	 * 2.检查图片长宽比例
+	 * @throws Exception
+	 * 其他case类似
+	 */
 	public void testBurst10P18M43() throws Exception{
 		try {
 			initUIAutomator(this.getName());
@@ -19,9 +25,8 @@ public class BurstCase extends Base{
 			common.startCamera();
 			common.deletePhoto();
 			
-			
 			CameraAction.configBurstImageSize(Camera.imsge_size[0]);
-			CameraAction.configBurstRate(Camera.burst[1]);
+			CameraAction.configBurstRate(Camera.burst[0]);
 			Boolean isExistSD=common.isExistSDCard();//判断是否存在SD卡
 			String burstPath=null;
 			if (isExistSD) {
@@ -31,7 +36,7 @@ public class BurstCase extends Base{
 			}
 			HashSet<String> beforeTakeVideoList = common.FileList(burstPath);
 			common.cameraKey();
-			common.waitTime(15);
+			sleep(12000);;
 			HashSet<String> afterTakeVideoList = common.FileList(burstPath);
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 			common.findViewById(one.hardware.Page.Camera.camera_setting_shortcut_id);
@@ -132,7 +137,7 @@ public class BurstCase extends Base{
 
 			HashSet<String> beforeTakeVideoList = common.FileList(burstPath);
 			common.cameraKey();
-			sleep(10000);
+			sleep(20000);
 			HashSet<String> afterTakeVideoList = common.FileList(burstPath);
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 
@@ -184,7 +189,7 @@ public class BurstCase extends Base{
 
 			HashSet<String> beforeTakeVideoList = common.FileList(burstPath);
 			common.cameraKey();
-			sleep(10000);
+			sleep(12000);
 			HashSet<String> afterTakeVideoList = common.FileList(burstPath);
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 
@@ -236,7 +241,7 @@ public class BurstCase extends Base{
 
 			HashSet<String> beforeTakeVideoList = common.FileList(burstPath);
 			common.cameraKey();
-			sleep(20000);
+			sleep(16000);
 			HashSet<String> afterTakeVideoList = common.FileList(burstPath);
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 
@@ -266,7 +271,7 @@ public class BurstCase extends Base{
 			common.handleException(e.getMessage());
 		}
 	}
-	public void testBurst30P3M43() throws Exception{
+	public void testBurst30P13M169() throws Exception{
 		String burstcfg = one.hardware.Page.Camera.burst[2];
 		String imgesz =one.hardware.Page.Camera.imsge_size[1];
 		try {
@@ -278,11 +283,18 @@ public class BurstCase extends Base{
 
 			CameraAction.configImageSize(imgesz);
 			CameraAction.configBurstRate(burstcfg);
+			Boolean isExistSD=common.isExistSDCard();//判断是否存在SD卡
+			String burstPath=null;
+			if (isExistSD) {
+				burstPath="/storage/sdcard1/Photo";
+			}else {
+				burstPath="/storage/sdcard0/Photo";
+			}
 
-			HashSet<String> beforeTakeVideoList = common.FileList("/sdcard/Photo");
+			HashSet<String> beforeTakeVideoList = common.FileList(burstPath);
 			common.cameraKey();
-			sleep(10000);
-			HashSet<String> afterTakeVideoList = common.FileList("/sdcard/Photo");
+			sleep(20000);
+			HashSet<String> afterTakeVideoList = common.FileList(burstPath);
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 
 			common.findViewById(one.hardware.Page.Camera.camera_setting_shortcut_id);
@@ -297,23 +309,21 @@ public class BurstCase extends Base{
 					double exp= 4/3;
 					if (hw==exp) {
 						common.infoLog(photopath+" -图片比列验证成功");
+						common.passcase();
 					}else {
 						common.infoLog(photopath+" -图片比列验证失败");
 						common.failcase(runcase);
 					}
 				}
 			}
-
 			//common.backToIdl();
-
-			common.passcase();
 			common.startLog( "*****End to run " + runcase + " *****");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			common.handleException(e.getMessage());
 		}
 	}
-	public void testBurst10P2M169() throws Exception{
+	public void testBurst10P8M169() throws Exception{
 		String burstcfg = one.hardware.Page.Camera.burst[0];
 		String imgesz =one.hardware.Page.Camera.imsge_size[2];
 		try {
@@ -325,11 +335,18 @@ public class BurstCase extends Base{
 
 			CameraAction.configImageSize(imgesz);
 			CameraAction.configBurstRate(burstcfg);
+			Boolean isExistSD=common.isExistSDCard();//判断是否存在SD卡
+			String burstPath=null;
+			if (isExistSD) {
+				burstPath="/storage/sdcard1/Photo";
+			}else {
+				burstPath="/storage/sdcard0/Photo";
+			}
 
-			HashSet<String> beforeTakeVideoList = common.FileList("/sdcard/Photo");
+			HashSet<String> beforeTakeVideoList = common.FileList(burstPath);
 			common.cameraKey();
-			sleep(10000);
-			HashSet<String> afterTakeVideoList = common.FileList("/sdcard/Photo");
+			sleep(12000);
+			HashSet<String> afterTakeVideoList = common.FileList(burstPath);
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 
 			common.findViewById(one.hardware.Page.Camera.camera_setting_shortcut_id);
@@ -344,23 +361,21 @@ public class BurstCase extends Base{
 					double exp= 16/9;
 					if (hw==exp) {
 						common.infoLog(photopath+" -图片比列验证成功");
+						common.passcase();
 					}else {
 						common.infoLog(photopath+" -图片比列验证失败");
 						common.failcase(runcase);
 					}
 				}
 			}
-
 			//common.backToIdl();
-
-			common.passcase();
 			common.startLog( "*****End to run " + runcase + " *****");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			common.handleException(e.getMessage());
 		}
 	}
-	public void testBurst20P2M169() throws Exception{
+	public void testBurst20P8M43() throws Exception{
 		String burstcfg = one.hardware.Page.Camera.burst[1];
 		String imgesz =one.hardware.Page.Camera.imsge_size[2];
 		try {
@@ -372,11 +387,18 @@ public class BurstCase extends Base{
 
 			CameraAction.configImageSize(imgesz);
 			CameraAction.configBurstRate(burstcfg);
+			Boolean isExistSD=common.isExistSDCard();//判断是否存在SD卡
+			String burstPath=null;
+			if (isExistSD) {
+				burstPath="/storage/sdcard1/Photo";
+			}else {
+				burstPath="/storage/sdcard0/Photo";
+			}
 
-			HashSet<String> beforeTakeVideoList = common.FileList("/sdcard/Photo");
+			HashSet<String> beforeTakeVideoList = common.FileList(burstPath);
 			common.cameraKey();
-			sleep(10000);
-			HashSet<String> afterTakeVideoList = common.FileList("/sdcard/Photo");
+			sleep(16000);
+			HashSet<String> afterTakeVideoList = common.FileList(burstPath);
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 
 			common.findViewById(one.hardware.Page.Camera.camera_setting_shortcut_id);
@@ -391,23 +413,21 @@ public class BurstCase extends Base{
 					double exp= 16/9;
 					if (hw==exp) {
 						common.infoLog(photopath+" -图片比列验证成功");
+						common.passcase();
 					}else {
 						common.infoLog(photopath+" -图片比列验证失败");
 						common.failcase(runcase);
 					}
 				}
 			}
-
 			//common.backToIdl();
-
-			common.passcase();
 			common.startLog( "*****End to run " + runcase + " *****");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			common.handleException(e.getMessage());
 		}
 	}
-	public void testBurst30P2M169() throws Exception{
+	public void testBurst30P8M43() throws Exception{
 		String burstcfg = one.hardware.Page.Camera.burst[2];
 		String imgesz =one.hardware.Page.Camera.imsge_size[2];
 		try {
@@ -419,11 +439,18 @@ public class BurstCase extends Base{
 
 			CameraAction.configImageSize(imgesz);
 			CameraAction.configBurstRate(burstcfg);
+			Boolean isExistSD=common.isExistSDCard();//判断是否存在SD卡
+			String burstPath=null;
+			if (isExistSD) {
+				burstPath="/storage/sdcard1/Photo";
+			}else {
+				burstPath="/storage/sdcard0/Photo";
+			}
 
-			HashSet<String> beforeTakeVideoList = common.FileList("/sdcard/Photo");
+			HashSet<String> beforeTakeVideoList = common.FileList(burstPath);
 			common.cameraKey();
-			sleep(10000);
-			HashSet<String> afterTakeVideoList = common.FileList("/sdcard/Photo");
+			sleep(20000);
+			HashSet<String> afterTakeVideoList = common.FileList(burstPath);
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 
 			common.findViewById(one.hardware.Page.Camera.camera_setting_shortcut_id);
@@ -435,16 +462,16 @@ public class BurstCase extends Base{
 				common.infoLog("expect图片总数：30张-Success");
 				for (String photopath : resultHashSet) {
 					double hw = common.getPicHeightWidth(photopath);
-					double exp= 16/9;
+					double exp= 4/3;
 					if (hw==exp) {
 						common.infoLog(photopath+" -图片比列验证成功");
+						common.passcase();
 					}else {
 						common.infoLog(photopath+" -图片比列验证失败");
 						common.failcase(runcase);
 					}
 				}
 			}
-			common.passcase();
 			common.startLog( "*****End to run " + runcase + " *****");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -453,8 +480,6 @@ public class BurstCase extends Base{
 	}
 	public static void main(String args[]){
 //		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.AccountCase", "", "2");
-		//new UiAutomatorHelper("AppSioeye", " one.test.ImageTestCase", "", "2");
-//		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.BurstDownToUp", "testBurstDownToUp", "2");
-		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.BurstCase", "testBurst10P18M43", "1");
+		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.BurstCase", "", "2");
 	}
 }
