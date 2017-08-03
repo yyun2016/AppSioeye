1eda5f7b88e8914c5b7d4b7bbe1d670b23b2cd5a
yun.yang <yun.yang@outlook.com>
2017-08-03 14:04:16 +0800

    common.waitTime(1210);

--- src/one/hardware/Testcase/Video_Slo_Mo_20m.java
+++ src/one/hardware/Testcase/Video_Slo_Mo_20m.java
@@ -7,7 +7,10 @@ import java.util.HashSet;
 import com.ckt.demo.UiAutomatorHelper;
 import one.hardware.Action.CameraAction;
 public class Video_Slo_Mo_20m extends Base{
-	public void testSloMo20m()throws Exception{
+	/**
+	 * 20min慢速录制自动停止
+	 * */
+	public void testSloMo20min()throws Exception{
 		try {
 			String navm = one.hardware.Page.Camera.nav_menu[4];
 			initUIAutomator(this.getName());
@@ -18,13 +21,18 @@ public class Video_Slo_Mo_20m extends Base{
 			HashSet<String> beforeTakeVideoList = common.FileList(CameraAction.getVideoPath());
 			common.cameraKey();
 			common.infoLog("等待20min");
-			common.waitTime(1201);
+			common.waitTime(1210);
 			HashSet<String> afterTakeVideoList = common.FileList(CameraAction.getVideoPath());
 			HashSet<String> resultHashSet = common.result(afterTakeVideoList, beforeTakeVideoList);
 			boolean lx1= common.findViewById2("com.hicam:id/recording_time2").exists();
-			if (lx1 == true || resultHashSet.size() != 1)
+			if (lx1||resultHashSet.size()!=1)
 			{
-				common.infoLog(" 自动关闭失败或者视频保存失败 ");
+				if (lx1) {
+					common.infoLog("recordingTimeExisting");
+				}
+				if (resultHashSet.size()!=1) {
+					common.infoLog("视频保存失败 ");
+				}
 				common.cameraKey();
 				common.failcase(runcase);
 			}
@@ -38,7 +46,7 @@ public class Video_Slo_Mo_20m extends Base{
 		}
 	}
 	public static void main(String args[]){
-		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.Video_Slo_Mo_20m", "test", "2");
+		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.Video_Slo_Mo_20m", "testSloMo20m", "2");
 	}
 
 }
