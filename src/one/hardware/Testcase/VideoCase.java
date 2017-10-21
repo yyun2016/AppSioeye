757b1cd9b1c72cc952eab55207365bcf68a1d27b
ruixiangxu <ruixiang.xu@ck-telecom.com>
2017-10-21 11:28:57 +0800

    修改videoCase方法，增加分辨率验证

--- src/one/hardware/Testcase/VideoCase.java
+++ src/one/hardware/Testcase/VideoCase.java
@@ -36,7 +36,7 @@ public class VideoCase extends Base{
  *   普通录像模式下设置不同的分辨率和视场角，录制视频，并检查     
  *         
  * */
-	private void videoQualityAngle(String quality,String angle) throws Exception{
+	private void videoQualityAngle(String quality,String angle,int height) throws Exception{
 		try {
 			initUIAutomator(this.getName());
 			common.startLog("*****Start to run " + runcase + " *****");
@@ -62,18 +62,23 @@ public class VideoCase extends Base{
 				String videopath = resultHashSet.iterator().next();
 				common.infoLog("new file:"+videopath);
 				String videoName = new File(videopath).getName();
-				common.VideoInfo(videopath);
-				FileManagerAction.playVideoByFileManager(videoName);
-				
-				if (common.findViewByText2("^Can't play this video.*").exists()) {
-					common.infoLog(videoName+" 播放失败" + "-Can't play this video");
-					common.findViewById2(Picture.button1_id).clickAndWaitForNewWindow();
-					common.failcase(runcase);
-					throw new Exception("FindObject" + "Can't play this video");
-				}else {
+				VideoNode activeNode = common.VideoInfo(videopath);
+				if (common.checkVideoInfo(height, activeNode)) {
+					common.infoLog("video info check success-"+videopath);
+				    FileManagerAction.playVideoByFileManager(videoName);
+				    if (common.findViewByText2("^Can't play this video.*").exists()) {
+				    	common.infoLog(videoName+" 播放失败" + "-Can't play this video");
+				    	common.findViewById2(Picture.button1_id).clickAndWaitForNewWindow();
+				    	common.failcase(runcase);
+				    	throw new Exception("FindObject" + "Can't play this video");
+				    }else {
 					common.infoLog(videoName+" 播放成功");
 					common.passcase();
-				}
+				    }
+				}else {
+					common.infoLog("video info check failed"+videopath);
+					common.failcase(runcase);
+				}   
 			}else {
 				common.failcase(runcase);
 			}
@@ -90,65 +95,76 @@ public class VideoCase extends Base{
 	public void testV48025fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[0];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV48060fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[1];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV480120fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[2];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV72025fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[3];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV72060fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[4];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV720120fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[5];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV108025fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[6];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV108060fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[7];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	
 	public void testV1080120fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[8];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	
 	public void testV2K25fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[9];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=2000;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV4K25fpsSuperWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[10];
 		String angle =one.hardware.Page.Camera.video_Angle[0];
-		videoQualityAngle(quality,angle);
+		int height=4000;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	
@@ -158,65 +174,76 @@ public class VideoCase extends Base{
 	public void testV48025fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[0];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV48060fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[1];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV480120fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[2];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV72025fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[3];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV72060fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[4];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV720120fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[5];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV108025fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[6];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV108060fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[7];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	
 	public void testV1080120fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[8];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	
 	public void testV2K25fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[9];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=2000;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV4K25fpsWide() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[10];
 		String angle =one.hardware.Page.Camera.video_Angle[1];
-		videoQualityAngle(quality,angle);
+		int height=4000;
+		videoQualityAngle(quality,angle,height);
 	}	
 	
 	/*
@@ -225,72 +252,83 @@ public class VideoCase extends Base{
 	public void testV48025fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[0];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV48060fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[1];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV480120fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[2];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=480;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV72025fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[3];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	public void testV72060fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[4];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV720120fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[5];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=720;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV108025fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[6];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV108060fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[7];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	
 	public void testV1080120fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[8];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=1080;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	
 	public void testV2K25fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[9];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=2000;
+		videoQualityAngle(quality,angle,height);
 	}
 	
 	public void testV4K25fpsMedium() throws Exception{
 		String quality = one.hardware.Page.Camera.video_quality[10];
 		String angle =one.hardware.Page.Camera.video_Angle[2];
-		videoQualityAngle(quality,angle);
+		int height=4000;
+		videoQualityAngle(quality,angle,height);
 	}
 		
 	
 
 
 	public static void main(String[] args){
-		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.VideoCase", "", "1");
+		new UiAutomatorHelper("AppSioeye", "one.hardware.Testcase.VideoCase", "testV4K25fpsMedium", "1");
 	}
 
 
